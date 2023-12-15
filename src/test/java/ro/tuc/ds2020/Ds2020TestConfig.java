7c614389404d875602eae73cb52d50d0986748a1
Haistachi <baz_adi83@yahoo.com>
2023-12-15 18:20:04 +0200

    start

--- src/test/java/ro/tuc/ds2020/Ds2020TestConfig.java
+++ src/test/java/ro/tuc/ds2020/Ds2020TestConfig.java
@@ -1,5 +1,6 @@
 package ro.tuc.ds2020;
 
+import chat.ChatApplicationBackEnd;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,7 +9,7 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = Ds2020Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = ChatApplicationBackEnd.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:application-test.properties")
 @AutoConfigureMockMvc
 public class Ds2020TestConfig {
